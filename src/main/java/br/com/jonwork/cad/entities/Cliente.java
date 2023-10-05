92387afd1da802a8eea2d5b4abb49bec0f63053c
jonassxd <jondudys@gmail.com>
2023-10-05 15:16:26 -0300

    Configurando a classe cliente, criando a tabela clientes

--- src/main/java/br/com/jonwork/cad/entities/Cliente.java
+++ src/main/java/br/com/jonwork/cad/entities/Cliente.java
@@ -1,7 +1,31 @@
 package br.com.jonwork.cad.entities;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Table(name="tb_clientes")
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class Cliente {
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private Integer idade;
+	private String cidade;
 }
