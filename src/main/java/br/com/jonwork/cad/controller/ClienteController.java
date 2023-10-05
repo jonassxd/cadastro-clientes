c1e9c9de0c1306ed643e49ef5a978e01fb4c7665
jonassxd <jondudys@gmail.com>
2023-10-05 15:46:00 -0300

    Criando o método para deletar cliente

--- src/main/java/br/com/jonwork/cad/controller/ClienteController.java
+++ src/main/java/br/com/jonwork/cad/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package br.com.jonwork.cad.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,23 +16,27 @@ import br.com.jonwork.cad.repositories.ClienteRepository;
 @RestController
 @RequestMapping
 public class ClienteController {
-	
+
 	@Autowired
 	private ClienteRepository repository;
-	
+
 	@PostMapping("/cadastrar")
 	public Cliente cadastrar(@RequestBody Cliente c) {
 		return repository.save(c);
 	}
-	
+
 	@GetMapping("/listar")
 	public Iterable<Cliente> listar() {
 		return repository.findAll();
 	}
-	
+
 	@PutMapping("/alterar")
 	public Cliente alterar(@RequestBody Cliente c) {
 		return repository.save(c);
 	}
-
+	
+	@DeleteMapping("/deletar/{id}")
+	public void deletar(@PathVariable Long id) {
+		repository.deleteById(id);
+	}
 }
