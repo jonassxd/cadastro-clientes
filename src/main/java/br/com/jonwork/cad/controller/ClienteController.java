72e7033531db4fdc765e131c32321e02351d4ad9
jonassxd <jondudys@gmail.com>
2023-10-05 15:52:32 -0300

    Adicionando o crossorigin para conectar com o front-end

--- src/main/java/br/com/jonwork/cad/controller/ClienteController.java
+++ src/main/java/br/com/jonwork/cad/controller/ClienteController.java
@@ -1,6 +1,7 @@
 package br.com.jonwork.cad.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import br.com.jonwork.cad.entities.Cliente;
 import br.com.jonwork.cad.repositories.ClienteRepository;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping
 public class ClienteController {
 
