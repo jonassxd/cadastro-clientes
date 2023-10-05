e02703c743fc813936faa9c0be8a00385a6c7e82
jonassxd <jondudys@gmail.com>
2023-10-05 15:26:17 -0300

    Criando o método para cadastrar clientes

--- src/main/java/br/com/jonwork/cad/controller/ClienteController.java
+++ src/main/java/br/com/jonwork/cad/controller/ClienteController.java
@@ -1,12 +1,26 @@
 package br.com.jonwork.cad.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.jonwork.cad.entities.Cliente;
+import br.com.jonwork.cad.repositories.ClienteRepository;
 
 @RestController
 @RequestMapping
 public class ClienteController {
+	
+	@Autowired
+	private ClienteRepository repository;
+	
+	@PostMapping("/cadastrar")
+	public Cliente cadastrar(@RequestBody Cliente c) {
+		return repository.save(c);
+	}
 	
 	@GetMapping("/")
 	public String ola() {
