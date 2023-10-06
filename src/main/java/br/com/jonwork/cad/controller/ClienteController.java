72d7ef70d80be7c7bb0244e018bf9a3b26c178b5
jonassxd <jondudys@gmail.com>
2023-10-06 18:10:16 -0300

    Adicionando a class WebConfig e configurando ela

--- src/main/java/br/com/jonwork/cad/controller/ClienteController.java
+++ src/main/java/br/com/jonwork/cad/controller/ClienteController.java
@@ -8,36 +8,35 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.jonwork.cad.entities.Cliente;
 import br.com.jonwork.cad.repositories.ClienteRepository;
 
 @RestController
-@CrossOrigin(origins = "*")
-@RequestMapping
+@CrossOrigin(origins = "http://localhost:4200")
+
 public class ClienteController {
 
 	@Autowired
 	private ClienteRepository repository;
 
-	@PostMapping("/cadastrar")
+	@PostMapping("/")
 	public Cliente cadastrar(@RequestBody Cliente c) {
 		return repository.save(c);
 	}
 
-	@GetMapping("/listar")
+	@GetMapping("/")
 	public Iterable<Cliente> listar() {
 		return repository.findAll();
 	}
 
-	@PutMapping("/alterar")
+	@PutMapping("/")
 	public Cliente alterar(@RequestBody Cliente c) {
 		return repository.save(c);
 	}
 	
-	@DeleteMapping("/deletar/{id}")
+	@DeleteMapping("/{id}")
 	public void deletar(@PathVariable Long id) {
 		repository.deleteById(id);
 	}
